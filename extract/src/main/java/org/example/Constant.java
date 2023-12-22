7976d93d0516ba3f744b99b7653c5fcb3b894220
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2023-12-22 13:00:50 +0700

    update

--- extract/src/main/java/org/example/Constant.java
+++ extract/src/main/java/org/example/Constant.java
@@ -6,6 +6,6 @@ public class Constant {
 
     public static final int ID_CONFIG_ONE = 1;
     public static final int ZERO = 0;
-    public static final int TWO = 0;
+    public static final int TWO = 2;
 
 }
